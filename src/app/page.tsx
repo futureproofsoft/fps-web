d00aad63962f4ecb52f87318f2f0d8eafbb32bef
Future Proof Soft <sava.tasic23@gmail.com>
2023-10-25 12:30:54 +0200

    feat: hero: frist look for hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import * as React from 'react';
 
 import ContactUs from '@/components/ContactUs';
+import { Hero } from '@/components/hero/hero';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
@@ -15,6 +16,7 @@ export default function HomePage() {
       <Head>
         <title>Hi</title>
       </Head>
+      <Hero />
       <ContactUs />
     </main>
   );
