598f02c9434b541523f490b322d7351a4fbc34a4
Future Proof Soft <sava.tasic23@gmail.com>
2023-12-07 12:31:38 +0100

    feat: feature/md-screen: adjust screen for md

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -42,15 +42,15 @@ export const Reviews = () => {
   };
 
   return (
-    <section id='reviews' className='3xl:py-36  lg:py-28'>
+    <section id='reviews' className='3xl:py-36  md:py-20 lg:py-28'>
       <div className='flex'>
-        <div className='z-10 flex w-[42%] flex-col justify-between'>
-          <div className='flex flex-col gap-5'>
-            <h5 className='border-greenText 3xl:text-2xl border-s-4 px-3 font-bold tracking-[4.8px] xl:text-lg'>
+        <div className='z-10 flex flex-col justify-between md:w-full lg:w-[42%]'>
+          <div className='flex flex-col  2xl:gap-10'>
+            <h5 className='border-greenText 3xl:text-2xl  border-s-4 px-3 font-bold tracking-[4.8px] md:text-sm xl:text-lg'>
               REVIEWS
             </h5>
 
-            <h2 className='3xl:text-6xl 3xl:leading-[64px] font-bold  xl:text-[38px]  xl:leading-[46px] 2xl:text-[44px]'>
+            <h2 className='3xl:text-6xl  font-bold  leading-none tracking-normal md:pb-5  md:pt-2 md:text-2xl  lg:pb-0 xl:text-[38px] 2xl:text-[44px]'>
               How <span className='text-greenText'>our clients</span> see us.
             </h2>
           </div>
@@ -67,18 +67,18 @@ export const Reviews = () => {
             />
           </div>
 
-          <div className='flex gap-[18px]'>
+          <div className='flex gap-[18px] md:pt-4 lg:pt-0'>
             <Arrow
               onClick={() => paginate(1)}
-              className='border-greenText 3xl:w-[50px] rotate-180 rounded-full border-[1.5px] hover:cursor-pointer lg:w-[35px] xl:w-[40px]'
+              className='border-greenText 3xl:w-[50px] rotate-180 rounded-full border-[1.5px] hover:cursor-pointer md:w-[35px] xl:w-[40px]'
             />
             <Arrow
               onClick={() => paginate(-1)}
-              className='border-greenText 3xl:w-[50px] rounded-full border-[1.5px] hover:cursor-pointer lg:w-[35px] xl:w-[40px]'
+              className='border-greenText 3xl:w-[50px] rounded-full border-[1.5px] hover:cursor-pointer md:w-[35px] xl:w-[40px]'
             />
           </div>
         </div>
-        <div className='z-0 w-[58%]'>
+        <div className='z-0 md:hidden lg:flex lg:w-[58%]'>
           <Image
             src={PhonePicture}
             className='h-full w-full'
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 export const Hero = () => {
   return (
-    <section className='3xl:py-36 flex items-center justify-between lg:py-20'>
+    <section className='3xl:py-36 flex items-center justify-between md:py-12 lg:py-20'>
       <div className='animation-slide-left w-1/2'>
         <h2 className='3xl:text-[68px] pb-6 font-bold leading-none lg:text-[40px] xl:text-[50px]  2xl:text-[55px]'>
           Let's turn your <span className='text-greenText'>idea</span> into{' '}
@@ -15,10 +15,10 @@ export const Hero = () => {
           </span>
           .
         </h2>
-        <p className='border-greenText 3xl:text-2xl border-s-4 px-3 lg:text-sm lg:leading-[16px]  xl:text-base  xl:leading-[20px] 2xl:text-lg 2xl:leading-[30px] '>
+        <p className='border-greenText 3xl:text-2xl border-s-4 px-3 md:text-xs lg:text-sm lg:leading-[16px]  xl:text-base  xl:leading-[20px] 2xl:text-lg 2xl:leading-[25px] '>
           We have the experience, knowledge, and technical know-how to
-          <br /> deliver an innovative digital product that will exceed your
-          expectations.
+          <br className='md:hidden lg:flex' /> deliver an innovative digital
+          product that will exceed your expectations.
         </p>
       </div>
       <div className='laptop-shadow animation-slide-right w-5/12 '>
