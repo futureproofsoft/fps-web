1468e290bd18a68bd93adcfec50433c3ac17dce3
Future Proof Soft <sava.tasic23@gmail.com>
2023-12-12 13:21:08 +0100

    feat: feature/fix-layout: add padding and add hidden for contact us

--- src/components/HowItWorks/WhoWeAre.tsx
+++ src/components/HowItWorks/WhoWeAre.tsx
@@ -4,13 +4,13 @@ export const WhoWeAre = () => {
   return (
     <section
       id='about-us'
-      className='3xl:pt-36  mb:py-12 sm:pt-14 lg:pt-20 xl:pt-28'
+      className='3xl:pt-36  mb:py-20 sm:pt-14 lg:pt-20 xl:pt-28'
     >
       <h5 className='border-greenText 3xl:text-2xl  border-s-4 px-3 font-bold tracking-[4.8px] sm:text-[12px] md:text-sm lg:text-base xl:text-lg'>
         WHO WE ARE
       </h5>
       <div className='mb:flex-col flex justify-between lg:flex-row'>
-        <h2 className='3xl:text-6xl lg:pb mb:pb-5 mb:pt-2 mb:text-2xl mb:leading-none font-bold  leading-none tracking-normal lg:pt-5 lg:text-[30px] xl:text-[38px] 2xl:pb-20 2xl:pt-7 2xl:text-[44px]'>
+        <h2 className='3xl:text-6xl lg:pb mb:pb-14 mb:pt-6 mb:text-2xl mb:leading-none font-bold  leading-none tracking-normal lg:pt-5 lg:text-[30px] xl:text-[38px] 2xl:pb-20 2xl:pt-7 2xl:text-[44px]'>
           Your <span className='text-greenText'>digital partner</span> to create
           <br className='sm:hidden lg:flex' /> a{' '}
           <span className='text-greenText'>recognizable</span> brand.
--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -6,13 +6,13 @@ export const Technologies = () => {
   return (
     <section
       id='technologies'
-      className='3xl:py-36 mb:py-12 sm:py-14 lg:py-20 xl:py-28'
+      className='3xl:py-36  mb:pt-20 sm:py-14 lg:py-20 xl:py-28'
     >
       <div>
-        <h5 className='border-greenText 3xl:text-2xl mb:text-sm mb:text-[12px] border-s-4 px-3 font-bold tracking-[4.8px] md:text-sm lg:text-base xl:text-lg'>
+        <h5 className='border-greenText 3xl:text-2xl mb:text-sm  border-s-4 px-3 font-bold tracking-[4.8px] md:text-sm lg:text-base xl:text-lg'>
           TECHNOLOGIES
         </h5>
-        <h2 className='3xl:text-6xl  mb:pb-12  mb:pt-2 mb:text-2xl mb:leading-none font-bold leading-none tracking-normal lg:pb-14 lg:pt-5 xl:text-[38px] 2xl:pb-20 2xl:pt-7 2xl:text-[44px]'>
+        <h2 className='3xl:text-6xl  mb:pb-12  mb:pt-6 mb:text-2xl mb:leading-none font-bold leading-none tracking-normal lg:pb-14 lg:pt-5 xl:text-[38px] 2xl:pb-20 2xl:pt-7 2xl:text-[44px]'>
           The <span className='text-greenText'>technologies</span> we apply to
           bring <span className='text-greenText'>your idea</span> to perfection.
         </h2>
