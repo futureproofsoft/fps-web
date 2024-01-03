3b3bed4bce1439ed6fdc8e0ce9c787f5eee878b3
Future Proof Soft <sava.tasic23@gmail.com>
2024-01-03 13:20:48 +0100

    feat: feature/services-animation: reviews update 2

--- src/components/Services/ServiceItem.tsx
+++ src/components/Services/ServiceItem.tsx
@@ -9,10 +9,10 @@ export const ServiceItem = ({ image, description }: ServiceItemProps) => {
   return (
     <div className='w-full'>
       <div className='border-greenText mb:w-[220px] flex flex-col rounded-3xl border-[3px] sm:w-[320px] md:w-full'>
-        <div className='3xl:my-20 mb:my-6 flex w-full justify-center align-middle sm:my-6 sm:px-6 lg:my-10 2xl:my-6'>
+        <div className='3xl:my-8 mb:my-6 flex w-full justify-center align-middle sm:my-6 sm:px-6 lg:my-10 2xl:my-6'>
           {image}
         </div>
-        <div className='border-greenText 3xl:text-5xl 3xl:my-[32px] 3xl:px-[26px] mb:mx-6 mb:mb-4 mb:px-[14px]  mb:text-base  border-s-4 font-bold sm:text-xl md:text-xl lg:mx-8 lg:mb-8  lg:px-[20px] lg:text-2xl  xl:text-3xl 2xl:mx-10   2xl:mb-10 2xl:px-[22px] 2xl:text-4xl 2xl:leading-[42px]'>
+        <div className='border-greenText 3xl:text-5xl 3xl:my-6 3xl:px-[26px] mb:mx-6 mb:mb-4 mb:px-[14px]  mb:text-base  border-s-4 font-bold sm:text-xl md:text-xl lg:mx-8 lg:mb-8  lg:px-[20px] lg:text-2xl  xl:text-3xl 2xl:mx-10   2xl:mb-10 2xl:px-[22px] 2xl:text-4xl 2xl:leading-[42px]'>
           {description}
         </div>
       </div>
