c138b2459dd1574a6c1514ce8c8ebd05e28772a0
Future Proof Soft <sava.tasic23@gmail.com>
2024-02-23 12:46:40 +0100

    feat: align items centre

--- src/components/ContactUs/Alert.tsx
+++ src/components/ContactUs/Alert.tsx
@@ -24,19 +24,22 @@ export const Alert: React.FC<AlertProps> = ({ labelUp, labelDown }) => {
   return (
     <>
       {isOpen && (
-        <div className='fixed inset-0 z-10 flex min-h-full w-screen flex-col items-center justify-center overflow-y-auto bg-black bg-opacity-70'>
-          <div className='mb:w-4/5 mb:h-1/5 bg-custom-gradient from-lightGreen border-blackBg to-darkBlue flex flex-col gap-4 rounded-lg px-4 pb-4 pt-5 sm:p-6 sm:pb-4 lg:h-1/4 lg:w-1/3'>
-            <div className='flex justify-end'>
-              <button
-                onClick={handleClose}
-                className='mb:w-[30px] p-1 focus:outline-none md:w-[40px]'
-              >
-                <Close alt='Close bar' />
-              </button>
-            </div>
-            <div className='mb:text-lg flex flex-col items-center justify-center text-white lg:text-2xl'>
-              <p>{labelUp}</p>
-              <p>{labelDown}</p>
+        <div>
+          <div className='fixed inset-0 z-10 flex min-h-full w-screen flex-col items-center justify-center bg-black bg-opacity-70'>
+            <div className='mb:w-4/5 mb:h-1/5 bg-custom-gradient from-lightGreen to-darkBlue rounded-lg p-2 lg:h-1/4 lg:w-1/3'>
+              <div className='relative'>
+                <button
+                  onClick={handleClose}
+                  className='mb:w-[30px] absolute right-0  top-0 p-1 focus:outline-none md:w-[40px]'
+                >
+                  <Close alt='Close bar' />
+                </button>
+              </div>
+
+              <div className='mb:text-lg flex h-full flex-col items-center justify-center text-white lg:text-2xl'>
+                <p>{labelUp}</p>
+                <p>{labelDown}</p>
+              </div>
             </div>
           </div>
         </div>
@@ -44,5 +47,3 @@ export const Alert: React.FC<AlertProps> = ({ labelUp, labelDown }) => {
     </>
   );
 };
-//   <p>Thanks for reaching out.</p>
-//   <p>We’ll get back to you soon!</p>
